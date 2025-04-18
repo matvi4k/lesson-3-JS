1d6f1289fa1e97fab9828ad5c80293ec00876980
MrDonex <sylenko.matvii@icloud.com>
2025-04-18 18:15:24 +0300

    dz

---
+++ js/javik.js
@@ -0,0 +1,15 @@
+let resuls = 5+5+'5';
+console.log(typeof resuls); // 1055
+
+const email = "matvey@gmail.com";
+console.log(email.includes('@')); // true
+
+const const1 = "My ";
+const const2 = "name ";
+const const3 = "is ";
+const fullName = const1 + const2 + const3 + 'Viktor';
+console.log(fullName); // My name is
+
+let userName = "Matvey";
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
